47406e9c03752c0315a43d58fb7abbd9b88e13b3
kimjoy0914 <kimjoy0914@gmail.com>
2025-04-24 16:32:38 +0900

    .gitignore파일 추가

---
+++ src/com/day1/MemberVariable.java
@@ -0,0 +1,10 @@
+package com.day1;
+
+public class MemberVariable {
+  int a = 10;
+
+  public static void main(String[] args) {
+    MemberVariable m = new MemberVariable();
+    System.out.println(m.a);
+  }
+}
